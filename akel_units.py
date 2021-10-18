7c31be3e077046b714185a388e8e7f5c7041394f
Denver <Denvernoell@gmail.com>
2021-10-18 13:57:57 -0700

    added file

---
+++ akel_units.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pyperclip
+import pint
+u = pint.UnitRegistry()
+
+
+st.title('Akel Units')
+
+
+def converter(f, t):
+    return u(f).to(u(t)).magnitude
+
+
+f = st.text_input('From')
+t = st.text_input('To')
+
+if t and f:
+    st.markdown(f'From ({f}) to ({t})')
+    c = converter(f, t)
+    st.markdown(c)
+    pyperclip.copy(c)
+
+# if st.button("Convert"):
+#     st.markdown(f'From {f} to {t}')
+#     c = converter(f, t)
+#     st.markdown(c)
+#     pyperclip.copy(c)
+
+# st.markdown(pint.UnitRegistry())
