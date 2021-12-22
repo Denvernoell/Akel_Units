79fdfdfd19697a85060d2de089c1f8242d2019f4
Denver Noell <65498186+Denvernoell@users.noreply.github.com>
2021-12-22 13:24:11 -0800

    Removed button

--- akel_units.py
+++ akel_units.py
@@ -27,42 +27,42 @@ if t and f:
 
 
 
-        st.components.v1.html('''
-<button class="button" onclick="myFunction()">Copy Conversion</button>
+#         st.components.v1.html('''
+# <button class="button" onclick="myFunction()">Copy Conversion</button>
 
 
-<script>
-function myFunction() {
-navigator.clipboard.writeText("'''+ str(c) +'''");
-}
-</script>
+# <script>
+# function myFunction() {
+# navigator.clipboard.writeText("'''+ str(c) +'''");
+# }
+# </script>
 
-<style>
-.button {
-  display: inline-block;
-  padding: 15px 25px;
-  font-size: 24px;
-  cursor: pointer;
-  text-align: center;
-  text-decoration: none;
-  outline: none;
-  color: #fff;
-  background-color: #4CAF50;
-  border: none;
-  border-radius: 15px;
-  box-shadow: 0 9px #999;
-}
+# <style>
+# .button {
+#   display: inline-block;
+#   padding: 15px 25px;
+#   font-size: 24px;
+#   cursor: pointer;
+#   text-align: center;
+#   text-decoration: none;
+#   outline: none;
+#   color: #fff;
+#   background-color: #4CAF50;
+#   border: none;
+#   border-radius: 15px;
+#   box-shadow: 0 9px #999;
+# }
 
-.button:hover {background-color: #3e8e41}
+# .button:hover {background-color: #3e8e41}
 
-.button:active {
-  background-color: #3e8e41;
-  box-shadow: 0 5px #666;
-  transform: translateY(4px);
-}
-</style>
+# .button:active {
+#   background-color: #3e8e41;
+#   box-shadow: 0 5px #666;
+#   transform: translateY(4px);
+# }
+# </style>
 
-        ''')
+#         ''')
 
 
 
